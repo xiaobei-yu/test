8988e0fd68955770f10caa39707f4c3945a9c735
xiaobei-yu <3085818080@qq.com>
2024-12-22 22:17:52 +0800

    iii

--- src/main/java/com/example/service/SpStudentService.java
+++
@@ -1,8 +0,0 @@
-package com.example.service;
-
-import com.example.pojo.SpStudent;
-import com.baomidou.mybatisplus.extension.service.IService;
-public interface SpStudentService extends IService<SpStudent>{
-
-
-}
